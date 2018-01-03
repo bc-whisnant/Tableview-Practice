3dc222b763b3ec552f4865c9c4ec57a9b4899acb
Whisnant <brandon.whisnant@xpta010166-mbp.hps.com>
2018-01-03 15:36:35 -0500

    first commit for practice app

--- tablePractice/ViewController.swift
+++ tablePractice/ViewController.swift
@@ -8,17 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let tasks = ["todo1", "todo2", "todo3", "todo4", "todo5"]
+    
+    //this has to match the identifier set for the prototype cell in storyboard
+    let cellReuseIdentifier = "todoCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //table code goes here
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //removed self reference here - previously self.tasks.count
+        return tasks.count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //removed self reference here - previously self.cellReuseIdentifier
+        let cell =  tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)!
+        
+        let taskList = tasks[(indexPath as NSIndexPath).row]
+        
+        cell.textLabel?.text = taskList
+        
+        return cell
+        
+    }
+    
+    
+    
+    
+    
 
 
 }
